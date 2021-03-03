5744c7db41b987f09d57921923ea3fb040ea49b8
Amos Setio <am05mhz@live.com>
2021-03-03 18:23:14 +0700

    change load method

--- src/LottiePlayer.vue
+++ src/LottiePlayer.vue
@@ -43,7 +43,7 @@ export default {
 					}
 				});
 			}
-			this.player.src = this.src;
+			this.player.load(this.src);
 			try {
 				this.$refs.LottiePlayer.appendChild(this.player);
 			} catch (ex) {
